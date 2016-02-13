b4feb576612f76cc34036cb6aa366317450bbc81
Christian Gonzalez <christian.d.gonzalez.92@gmail.com>
2016-02-12 18:10:57 -0800

    updated login/signup and navbars

--- client/js/controllers/loginPageController.js
+++ client/js/controllers/loginPageController.js
@@ -3,6 +3,32 @@ vibe.controller("loginController", function ($scope, $location, $routeParams, $d
 //function to send form information to user/studio factory, route to profile page when successful
 
 //
+$scope.dropDown = true;
 
+$scope.scrollTo = function(id) {
+      var thisLocation = $location.hash(id);
+      var someElement = angular.element(document.getElementById(id));
+      console.log(thisLocation)
+      console.log(someElement)
+    $document.scrollToElementAnimated(someElement);
+   }
+$scope.appendToEl = angular.element(document.querySelector('#toggleButton'))
+
+$(window).on("resize.doResize", _.throttle(function (){
+ //if the window goes beyond reformatting size
+         if (window.innerWidth > 767) {
+             $scope.$apply(function(){
+                 //if the dropdown is active (meaning hidden)
+                 if ($scope.dropDown == true) {
+                     return
+                 } else {
+                     //otherwise disable it
+                     $scope.dropDown = !$scope.dropDown
+                 }
+             });
+         }
+
+
+     },100));
 
 })
