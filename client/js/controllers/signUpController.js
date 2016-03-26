3ea687362d123d6554a6d61ec6dae523e35de9e6
Christian Gonzalez <christian.d.gonzalez.92@gmail.com>
2016-03-26 11:33:35 -0700

    password verification

--- client/js/controllers/signUpController.js
+++ client/js/controllers/signUpController.js
@@ -4,13 +4,25 @@ vibe.controller("signUpController", function ($scope, $location, $routeParams, $
 	$scope.isLoggedIn = auth.isLoggedIn;
 
 	$scope.signUp = function() {
-    auth.register($scope.newUser).error(function(error){
-	        $scope.error = error;
-	    })
-		.then(function(output){
-	        $scope.currentUser = auth.currentUser()
-			$scope.userName = output.data.user.username
-	        $location.path('/profile/'+$scope.userName).search({user: output.data.user});
-	    })
+		if ($scope.newUser.password != $scope.newUser.passwordCheck){
+			$scope.pwFailed = true;
+			return
+		} else if ($scope.newUser.password === undefined || $scope.newUser.email === undefined || $scope.newUser.userName === undefined){
+			console.log($scope.newUser)
+			$scope.tryAgain  = true;
+			return
+		}
+		else {
+			$scope.pwFailed = false;
+			$scope.tryAgain = false;
+			auth.register($scope.newUser).error(function(error){
+					$scope.error = error;
+				})
+				.then(function(output){
+					$scope.currentUser = auth.currentUser()
+					$scope.userName = output.data.user.username
+					$location.path('/profile/'+$scope.userName).search({user: output.data.user});
+				})
+		}
 	}
 })
